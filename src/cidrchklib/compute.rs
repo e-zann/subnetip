7a2509bc23cc364354fb7ff52b339a241dbfd770
dizo <zann@arklab.dev>
2022-09-20 18:52:06 +0200

    compute file

---
+++ src/cidrchklib/compute.rs
@@ -0,0 +1,15 @@
+
+
+pub fn check_a_pair(test_ip: &str, subnet_ip: &str, cidr_length: &u8) -> bool {
+
+	for n in 0 .. *cidr_length {
+
+		if test_ip.chars().nth(n.into()).unwrap() != subnet_ip.chars().nth(n.into()).unwrap() {
+
+			return false;
+
+		}
+	}
+
+	true
+}
